c05cd863ab75d51d492986e884b003391b9f888f
v-rowjee <vedrowjee@gmail.com>
2022-03-02 21:55:26 +0400

    remove admin

--- DinasAdmin/includes/footer.php
+++
@@ -1,9 +0,0 @@
-    <!-- Bootstrap -->
-    <script
-      src="../bootstrap/bootstrap.bundle.min.js"
-      type="text/javascript"
-    ></script>
-    <!-- My script -->
-    <script src="js/script.js" type="text/javascript"></script>
-  </body>
-</html>
