38735259ca4885f97cad136e95521e81e847d61d
v-rowjee <vedrowjee@gmail.com>
2022-03-02 09:30:46 +0400

    .

--- login.php
+++ login.php
@@ -62,7 +62,7 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
                 $_SESSION['rid'] = $user['rid'];
 
                 if($user['is_admin']=='yes'){ // admin
-                    header('location: /dinasadmin/dashboard.php');
+                    header('location: dinasadmin/dashboard.php');
                     die();
                 }else{ // normal user
                     header('location: index.php');
