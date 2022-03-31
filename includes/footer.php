176b760d0b12bbc070d19ae2919d7dfe4486093c
v-rowjee <vedrowjee@gmail.com>
2022-03-31 21:17:13 +0400

    footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,106 +1,134 @@
   <!-- Footer -->
-  <footer class="footer py-4" >
-    <div class="container text-center text-md-start">
-        <div class="row text-center text-md-start">
-            <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
-                <h5 class="text mb-4 font-weight-bold" style="color: #B4A064;">Dina's Restaurant</h5>
-                <p>about dina's.</p>
+  <footer class=" text-white pt-5 pb-4" >
+            <!-- <div class="waveWrapper waveAnimation">
+                <div class="waveWrapperInner bgTop">
+                  <div class="wave waveTop" style="background-image: url('http://front-end-noobs.com/jecko/img/wave-top.png')"></div>
+                </div>
+                <div class="waveWrapperInner bgMiddle">
+                  <div class="wave waveMiddle" style="background-image: url('http://front-end-noobs.com/jecko/img/wave-mid.png')"></div>
+                </div>
+                <div class="waveWrapperInner bgBottom">
+                  <div class="wave waveBottom" style="background-image: url('http://front-end-noobs.com/jecko/img/wave-bot.png')"></div>
+                </div>
+              </div> -->
+        <div class="container text-center text-md-start">
+            <div class="row text-center text-md-start">
+                <div class="col-12 col-md-3 mx-auto mt-1">
+                    <h5 class="text mb-4 font-weight-bold" style="color: #B4A064;"> <img src="images/logo.png" class="avatar" style="filter: invert(68%) sepia(28%) saturate(559%) hue-rotate(7deg) brightness(90%) contrast(84%);"></h5>
+                    <p>An elegant five-star resort in Mauritius, Dinarobin Beachcomber Golf Resort & Spa is a haven of peace and tranquility - the ideal setting for your luxury holiday in Mauritius and the perfect sanctuary to renew your body and soul.</p>
 
+                </div>
+                <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+                    <h5 class="text-uppercase mb-4 font-weight-bold" style="color: #B4A064;">Features Available</h5>
+                    <p>
+                        <a href="#"class="text-white" style="text-decoration: none;">Reservations</a>
+                    </p>
+                    <p>
+                        <a href="#" class="text-white" style="text-decoration:none;"> Book a table</a>
+                    </p>
+                    <p>
+                        <a href="#" class="text-white" style="text-decoration:none;"> Menu</a>
+                    </p>
+                    <p>
+                        <a href="#" class="text-white" style="text-decoration:none;"> Profiles</a>
+                    </p>
+                    
+                </div>
+
+                <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
+                    <h5 class="text-uppercase mb-4 font-weight-bold" style="color: #B4A064;">useful links</h5>
+                    <p>
+                        <a href="#"class="text-white" style="text-decoration: none;">Admin</a>
+                    </p>
+                    <p>
+                        <a href="#" class="text-white" style="text-decoration:none;"> Affiliates</a>
+                    </p>
+                    <p>
+                        <a href="#" class="text-white" style="text-decoration:none;"> Opening Hours</a>
+                    </p>
+                    <p>
+                        <a href="#" class="text-white" style="text-decoration:none;"> Help</a>
+                    </p>
+                    <p>
+                        <a href="#" class="text-white" style="text-decoration:none;"> Contacts</a>
+                    </p>
+                </div>
+
+                <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
+                    <h5 class="text-uppercase mb-4 font-weight-bold" style="color: #B4A064;">Contact Us</h5>
+                    <p class="nowrap">
+                        <i class="fas fa-home me-3"></i> Beachcomber,Le Morne,Mauritius
+                    </p>
+                    <p class="nowrap">
+                        <i class="fas fa-envelope me-3"></i>Dinasrestaurant.test@gmail.com
+                    </p>
+                    <p class="nowrap">
+                        <i class="fas fa-phone me-3"></i>  (+230) 407-9000
+                    </p>
+                    <p class="nowrap">
+                        <i class="fas fa-print me-3"></i> (+230) 209-1600
+                    </p>
+
+    
             </div>
-            <div class="col-12 col-md-3 mt-3">
-                <h5 class="text-uppercase mb-4 font-weight-bold" style="color: #B4A064;">Menu</h5>
-                <p>
-                    <a href="#"class="text-white" style="text-decoration: none;">Reservations</a>
-                </p>
-                <p>
-                    <a href="#" class="text-white" > Book a table</a>
-                </p>
-                <p>
-                    <a href="#" class="text-white" > Source File</a>
-                </p>
-                <p>
-                    <a href="#" class="text-white" > Bootstrap5</a>
-                </p>
-                
-            </div>
-
-            <div class="col-12 col-md-3 mt-3">
-                <h5 class="text-uppercase mb-4 font-weight-bold text-gold">useful links</h5>
-                <p>
-                    <a href="#"class="text-white">admin</a>
-                </p>
-                <p>
-                    <a href="#" class="text-white" > affiliates</a>
-                </p>
-                <p>
-                    <a href="#" class="text-white" > Opening Hours</a>
-                </p>
-                <p>
-                    <a href="#" class="text-white" > help</a>
-                </p>
-                <p>
-                    <a href="#" class="text-white" > The Providers</a>
-                </p>
-            </div>
-
-            <div class="col-12 col-md-3 mt-3">
-                <h5 class="text-uppercase mb-4 font-weight-bold text-gold">Contact Us</h5>
-                <p>
-                    <i class="fas fa-home me-3"></i> Beachcomber,Le Morne,Mauritius
-                </p>
-                <p>
-                    <i class="fas fa-envelope me-3"></i>dinasrestaurant.test@gmail.com
-                </p>
-                <p>
-                    <i class="fas fa-phone me-3"></i>  (+230) 407-9000
-                </p>
-                <p>
-                    <i class="fas fa-print me-3"></i> +230rgwr
-                </p>
-
-
         </div>
-    </div>
-        <hr class="mb-4 text-gold">
-        <div class="row align-items-center">
-        <div class="col-md-7 col-lg-5">
-            <p class="nowrap">Copyright @2022 All right reserves by:
-                <a href="#" style= "text-decoration: none;">
-                <strong  style="color: #B4A064;">Dina Robin</strong>
-            </a></p>
+            <hr class="mb-4" style="color: #B4A064;">
+            <div class="row align-items-center">
+            <div class="col-md-7 col-lg-5">
+                <p>Copyright @2022 All rights reserved by:
+                    <a href="#" style= "text-decoration: none;">
+                    <strong  style="color: #B4A064;">Dina Robin</strong>
+                </a></p>
 
 
-        </div>
+            </div>
+            
+            <div class="col-md-7 mx-auto  ">
+                <div class="text-center text-right">
+                    <ul class="list-unstyled list-inline">
+                        <li class="list-inline-item">
+                            <a href="#" class="btn-floating btn-sm" style="font-size: 25px;color: #B4A064;"><i class="fab fa-facebook"></i>
+                            </a>
+                        </li>
+                        <li class="list-inline-item">
+                            <a href="#" class="btn-floating btn-sm" style="font-size: 25px;color: #B4A064;"><i class="fab fa-twitter"></i>
+                            </a>
+                        </li><li class="list-inline-item">
+                            <a href="#" class="btn-floating btn-sm" style="font-size: 25px;color: #B4A064;"><i class="fab fa-instagram"></i>
+                            </a>
+                        </li><li class="list-inline-item">
+                            <a href="#" class="btn-floating btn-sm" style="font-size: 25px;color: #B4A064;"><i class="fab fa-google"></i>
+                            </a>
+                        </li><li class="list-inline-item">
+                            <a href="#" class="btn-floating btn-sm" style="font-size: 25px ;color: #B4A064;"><i class="fab fa-linkedin-in"></i>
+                            </a>
+                        </li><li class="list-inline-item">
+                            <a href="#" class="btn-floating btn-sm" style=" font-size:25px ;color: #B4A064;"><i class="fab fa-youtube"></i>
+                            </a>
+                        </li> 
+                    </ul>
+                </div>
 
-        <div class="col-md-5 col-lg-7 ">
-            <div class="text-center text-right">
-                <ul class="list-unstyled list-inline">
-                    <li class="list-inline-item">
-                        <a href="#" class="btn-floating btn-sm" style="font-size: 25px;color: #B4A064;"><i class="fab fa-facebook"></i>
-                        </a>
-                    </li>
-                    <li class="list-inline-item">
-                        <a href="#" class="btn-floating btn-sm" style="font-size: 25px;color: #B4A064;"><i class="fab fa-twitter"></i>
-                        </a>
-                    </li><li class="list-inline-item">
-                        <a href="#" class="btn-floating btn-sm" style="font-size: 25px;color: #B4A064;"><i class="fab fa-instagram"></i>
-                        </a>
-                    </li><li class="list-inline-item">
-                        <a href="#" class="btn-floating btn-sm" style="font-size: 25px;color: #B4A064;"><i class="fab fa-google"></i>
-                        </a>
-                    </li><li class="list-inline-item">
-                        <a href="#" class="btn-floating btn-sm" style="font-size: 25px ;color: #B4A064;"><i class="fab fa-linkedin-in"></i>
-                        </a>
-                    </li><li class="list-inline-item">
-                        <a href="#" class="btn-floating btn-sm" style=" font-size:25px ;color: #B4A064;"><i class="fab fa-youtube"></i>
-                        </a>
-                    </li> 
-                </ul>
             </div>
 
-        </div>
-                                
-    </footer>
+            
+                                    
+        </footer>
+        <!--Waves Container-->
+        <div class="position-relative w-100">
+                <svg class="waves" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 24 150 28" preserveAspectRatio="none" shape-rendering="auto">
+                    <defs>
+                        <path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
+                    </defs>
+                    <g class="parallax">
+                        <use xlink:href="#gentle-wave" x="48" y="0" fill="rgba(0,0,0,0.7)" />
+                        <use xlink:href="#gentle-wave" x="48" y="3" fill="rgba(0,0,0,0.5)" />
+                        <use xlink:href="#gentle-wave" x="48" y="5" fill="rgba(0,0,0,0.3)" />
+                        <use xlink:href="#gentle-wave" x="48" y="7" fill="#111" />
+                    </g>
+                </svg>
+            </div>
+            <!--Waves end-->
     <!-- JQuery -->
     <script src="js/jquery-3.6.0.min.js"></script>
 
