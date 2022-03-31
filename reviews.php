7c8ecd21fa60816824fdaa29ae417d995e3d5ff6
v-rowjee <vedrowjee@gmail.com>
2022-03-31 22:12:42 +0400

    reviews and img

---
+++ reviews.php
@@ -0,0 +1,76 @@
+<!doctype html>
+<html lang="en">
+  <head>
+  	<title>Reviews</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700&display=swap" rel="stylesheet">
+
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+	
+	<link rel="stylesheet" href="css/default.css">
+    <link rel="stylesheet" href="css/reviews.css">
+
+	</head>
+    <body>
+       <div class="profile-area">
+           <div class="container">
+               <div class="row">
+                   <div class="col-md-4">
+                       <div class="card">
+                           <div class="img1"><img src="images/dadh.png" alt=""></div>
+                           <div class="img2"><img src="images/person1.jpg" alt=""></div>
+                           <div class="main-text" style="color: black;">
+                               <h2>Henry Legrand</h2>
+                               <p>I had a great time eating delicious food at your restaurant. The waiters were committed to great service and were very friendly. The atmosphere was awesome and i definitely liked that you have a modern style. Every food on your menu tasted great.</p>
+                               
+                           </div>
+                           <div class="socials">
+                               <i class="fa fa-facebook"></i>
+                               <i class="fa fa-linkedin"></i>
+                               <i class="fa fa-dribbble"></i>
+                               <i class="fa fa-twitter"></i>
+                           </div>
+                       </div>
+                   </div>
+                   <div class="col-md-4">
+                    <div class="card">
+                        <div class="img1"><img src="images/dadh.png" alt=""></div>
+                        <div class="img2"><img src="images/person2.jpg" alt=""></div>
+                        <div class="main-text" style="color: black;">
+                            <h2>Max Dillion</h2>
+                            <p>This place was amazing! The food was absolutely delicious. (10/10) would recommend this place again.</p>
+
+                        </div>
+                        <div class="socials">
+                            <i class="fa fa-facebook"></i>
+                            <i class="fa fa-linkedin"></i>
+                            <i class="fa fa-dribbble"></i>
+                            <i class="fa fa-twitter"></i>
+                        </div>
+                    </div>
+                </div>
+                   <div class="col-md-4">
+                    <div class="card">
+                        <div class="img1"><img src="images/dadh.png" alt=""></div>
+                        <div class="img2"><img src="images/person3.jpg" alt=""></div>
+                        <div class="main-text" style="color: black;">
+                            <h2>Isabella Harper</h2>
+                            <p>We had the beef and spinach curry tonight and it was "THE BEST CURRY" I've ever had. Thank you again for welcoming us, see you soon. ^^</p>
+
+                        </div>
+                        <div class="socials">
+                            <i class="fa fa-facebook"></i>
+                            <i class="fa fa-linkedin"></i>
+                            <i class="fa fa-dribbble"></i>
+                            <i class="fa fa-twitter"></i>
+                        </div>
+                    </div>
+                </div>
+               </div>
+           </div>
+       </div>
+    </body>
+
+</html>
