631c9776f2b39496d2e1699cbe73ab82c05ba336
v-rowjee <vedrowjee@gmail.com>
2022-05-23 22:11:18 +0400

    google sign in api

--- config/send_mail.php
+++
@@ -1,7 +0,0 @@
-<?php
-$to = "vedrowjee@gmail.com";
-$subject = "Getting In Touch";
-$txt = '"' . $_POST["message"] . ' \nFrom ' . $_POST["name"] . '"';
-$headers = "From: " . $_POST["email"] . "";
-
-mail($to, $subject, $txt, $headers);
