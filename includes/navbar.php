8d736a695e246d591c1fb6e955601ea0abb7631e
v-rowjee <vedrowjee@gmail.com>
2022-06-09 23:15:55 +0400

    btn hover

--- includes/navbar.php
+++ includes/navbar.php
@@ -99,7 +99,7 @@
 
 <!-- SOUNDCLOUD -->
 <iframe id="soundcloud" width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" class="mt-5" 
-    src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/345582787&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true">
+    src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/345582787&color=%23ff5500&auto_play=false&hide_related=true&show_comments=false&show_user=false&show_reposts=false&show_teaser=false&visual=false">
 </iframe>
 
 <script>
