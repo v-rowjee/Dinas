a659194c24066e10cb3cf2b8b27986e2d6fed391
v-rowjee <vedrowjee@gmail.com>
2022-07-06 14:53:41 +0400

    menu json function

--- includes/menu-item.php
+++ includes/menu-item.php
@@ -1,25 +1,27 @@
 <?php
 
+// function to get menu cards of a category
 function getMenu($category){
-    $filename = '../dinasadmin/json/menu.json';
-    $data = file_get_contents($filename);
-    $json = json_decode($data,true);
 
-    foreach($json as $menu){
-        if(strtolower($menu['category']) == $category){
+    $json = getData();  // function to get all menu from DB in json format
+
+    $data = json_decode($json,false);   // Convert JSON into PHP object
+
+    foreach($data as $menu){
+        if(strtolower($menu->category) == $category){
             echo '
                 <div class="col-sm-6 col-lg-3 col-xxl-3">
                     <div class="card card-shadow card-tilt">
                     <img
-                        src="/dinasadmin/images/menu/'.$menu['img'].'"
+                        src="/dinasadmin/images/menu/'.$menu->img.'"
                         class="card-img-top"
-                        alt="'.$menu['name'].'"
+                        alt="'.$menu->name.'"
                     />
                     <div class="card-body">
-                        <h5 class="card-title nowrap">'.$menu['name'].'</h5>
-                        <p class="card-text">'.$menu['caption'].'</p>
+                        <h5 class="card-title nowrap">'.$menu->name.'</h5>
+                        <p class="card-text">'.$menu->caption.'</p>
                         <div class="d-flex">
-                            <h6 class="price">Rs '.$menu['price'].'</h6>
+                            <h6 class="price">Rs '.$menu->price.'</h6>
                         </div>
                     </div>
                     </div>
@@ -29,17 +31,32 @@ function getMenu($category){
     }
 }
 
+// function to get a list of menu names for a category
 function getMenuList($category){
-    $filename = '../dinasadmin/json/menu.json';
-    $data = file_get_contents($filename);
-    $json = json_decode($data,true);
+
+    $json = getData();
+
+    $data = json_decode($json,false);
 
     $items = array();
-    foreach($json as $menu){
-        if(strtolower($menu['category']) == $category){
-            array_push($items, $menu['name']);
+    foreach($data as $menu){
+        if(strtolower($menu->category) == $category){
+            array_push($items, $menu->name);
         }
     }
     $list = implode(' - ', $items);
     echo $list;
+}
+
+// function to get all menu from DB in json format
+function getData(){
+
+    require 'config/db_connect.php';
+
+    $sql = "SELECT * FROM menu";
+    $query = $conn->prepare($sql);
+    $query->execute();
+    $data = $query->fetchAll(PDO::FETCH_ASSOC);
+
+    return json_encode($data);
 }
